3c43ef16aad655e1c617a255b85e4c88a51b0ef2
Szymon Wójcik <cersin96@gmail.com>
2021-10-28 11:36:04 +0200

    boilerplate

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import AddCurrency from "../views/AddCurrency";
+import CurrencyConverter from "@/views/CurrencyConverter";
+import CurrencyTable from "@/views/CurrencyTable";
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'AddCurrency',
+    component: AddCurrency
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/currencyTable',
+    name: 'CurrencyTable',
+    component: CurrencyTable
+  },
+  {
+    path: '/currencyConverter',
+    name: 'CurrencyConverter',
+    component: CurrencyConverter
+  },
 ]
 
 const router = createRouter({
