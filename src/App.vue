61f2c2cbd95c20c0503d7c3911322c2698de15f3
Szymon Wójcik <cersin96@gmail.com>
2021-10-28 17:07:06 +0200

    remove redundant code

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,11 @@
 
 <style lang="scss">
 @import "styles/base";
-
-.navigation {
-  &-enter-active, &-leave-active {
-    transition: all 2s ease-in-out;
-  }
-
-  &-enter, &-leave-to {
-    transform: translateX(-100vw);
-  }
-}
 </style>
 <script>
 import Sidenav from "@/components/UI/Sidenav";
 import Breadcrumb from "@/components/UI/Breadcrumb";
 export default {
-  components: {Breadcrumb, Sidenav},
-  data() {
-    return {
-      menuOpen: false
-    }
-  }
+  components: {Breadcrumb, Sidenav}
 }
 </script>
